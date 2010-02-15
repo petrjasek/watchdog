6e2fab23cc3c89854eb210b18430f420d8e693ed
Petr Jašek <jasekpetr@gmail.com>
2010-02-15 16:51:48 +0100

    Get and filter feed

--- watchdog.py
+++ watchdog.py
@@ -2,48 +2,49 @@
 # Watchdog
 
 FEED = 'http://www.eshop-simecek.cz/xml_feed_all.php'
-FILE = 'feed.xml'
 
-import random
-import urllib2
-import xml
+import urllib2 as urllib
 import xml.dom.minidom as minidom
-
-def updateFeed():
-	"""Gets feed and stores it"""
-	feed = urllib2.urlopen(FEED)
-	file = open(FILE, 'w')
-	for line in feed:
-		file.write(line.decode('windows-1250').encode('utf-8'))
-	file.close()
-
-def getFeed(filename):
-	"""Load feed and decode it"""
-	f = open(filename, 'r')
 
 def getName(name):
 	"""Get product name"""
-	return name.childNodes[0].data
+	return name.childNode.data
 
 def getPrice(price):
 	"""Get product price"""
-	return price.childNodes[0].data
+	return price.childNode.data
 
-# update feed
-#updateFeed()
+def checkPrice(name, price):
+	"""Checks price of product"""
+	params = {'q': name, 'order': 'price', 'minPrice': 0, 'maxPrice': price}
+	url = 'http://zbozi.cz/items?' + urllib.urlencode(params)
+	sock = urllib.urlopen(url)
+	html = sock.read()
+	sock.close()
+
+	print(url)
+	print(html)
+
+# get feed
+feed = ""
+sock = urllib.urlopen(FEED)
+for line in sock:
+	if line.find('SHOP') >= 0 or line.find('PRODUCT') >= 0 or line.find('PRICE') >= 0:
+		feed += line.decode('windows-1250').encode('utf-8')
+sock.close()
+
+print feed
+exit()
 
 # parse feed
-
-try:
-	feed = minidom.parse(FILE)
-except xml.parsers.expat.ExpatError:
-	pass
+doc = minidom.parseString(feed)
 
 # get products
-for item in feed.getElementsByTagName('SHOPITEM'):
+for item in doc.getElementsByTagName('SHOPITEM'):
 	name = getName(item.getElementsByTagName('PRODUCT')[0])
 	price = getPrice(item.getElementsByTagName('PRICE_VAT')[0])
-	print(name, price)
+	checkPrice(name, price)
+	exit()
 
 # gc
-feed.unlink()
+doc.unlink()
